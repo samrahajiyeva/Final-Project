9973b797139ee840c74268a5da601d13ea5841bb
samrahajiyeva <universes.girl15@gmail.com>
2023-06-27 17:26:33 +0400

    commit

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,18 @@ const listingRoute = require('./app/routes/listing.route')
 const activityRoute = require('./app/routes/activity.route')
 const blogRoute = require('./app/routes/blog.route')
 const shopRoute = require('./app/routes/shop.route')
-const userRoute = require('./app/routes/user.route')
+const userRoute = require('./app/routes/AuthRoutes')
+app.use('/api/users',require('./app/routes/AuthRoutes'))
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+app.use(express.json({ limit: "20mb" }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+
+mongoose.set("strictQuery", true);
+
 const path = require('path')
 const uploadPath=path.join(__dirname,'public')
 app.use('/public',express.static(uploadPath))
@@ -23,7 +34,7 @@ app.use('/listing',listingRoute)
 app.use('/activity',activityRoute)
 app.use('/blogs',blogRoute)
 app.use('/shop',shopRoute)
-app.use('/user',userRoute)
+app.use('/auth',userRoute)
 
 app.listen(8080, () => {
     console.log("server running")
--- frontend/src/pages/Admin/AdminRoot/AdminRoot.jsx
+++ frontend/src/pages/Admin/AdminRoot/AdminRoot.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from '../../../layout/Admin/Header/Header'
-import { Outlet  } from "react-router-dom";
+import { Outlet, useNavigate  } from "react-router-dom";
 import Footer from '../../../layout/Admin/Footer/Footer';
-
+import axios from 'axios'
+import {useCookies} from 'react-cookie'
 function AdminRoot() {
+  const [cookies, setCookie, removeCookie] = useCookies(["jwt"]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const verifyUser = async () => {
+      if (!cookies.jwt) {
+        navigate("/login");
+      } else {
+        const { data } = await axios.post(
+          "http://localhost:8080/auth/login",
+          {},
+          {
+            withCredentials: true,
+          }
+        );
+      }
+    };
+
+    verifyUser();
+  }, [cookies, removeCookie, navigate]);
   return (
     <>
     <Header />
