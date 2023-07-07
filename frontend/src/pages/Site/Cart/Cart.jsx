41c4e5f3adf4839a9afb6f41372ba0821fcbb713
samrahajiyeva <universes.girl15@gmail.com>
2023-07-08 00:20:50 +0400

    commit

--- frontend/src/pages/Site/Cart/Cart.jsx
+++ frontend/src/pages/Site/Cart/Cart.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeFromCart } from '../../../redux/ShoppingSlice/ShoppingSlice';
+import { removeFromCart, addToCart } from '../../../redux/ShoppingSlice/ShoppingSlice';
 import Loading from '../../../components/Site/Loading/Loading';
 import './Cart.scss';
 import { Link, useNavigate } from 'react-router-dom';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { FaPlus, FaMinus } from 'react-icons/fa';
 
 function Cart() {
   const items = useSelector((state) => state.shoppingcart.items);
@@ -26,13 +27,28 @@ function Cart() {
   }, [items]);
 
   const filteredItems = items.filter((item) =>
-    item.title.toLowerCase().includes(searchQuery.toLowerCase())
+    item.title && item.title.toLowerCase().includes(searchQuery.toLowerCase())
   );
+
 
   let totalPrice = 0;
   filteredItems.forEach((item) => {
-    totalPrice += item.price;
+    totalPrice += item.price * item.count;
   });
+
+  const handleIncrement = (item) => {
+    dispatch(addToCart({ id: item.id, count: item.count + 1 }));
+  };
+
+  const handleDecrement = (item) => {
+    if (item.count === 1) {
+      dispatch(removeFromCart(item.id));
+    } else {
+      dispatch(addToCart({ id: item.id, count: item.count - 1 }));
+    }
+  };
+  
+
 
   return (
     <>
@@ -63,9 +79,7 @@ function Cart() {
                   value={searchQuery}
                   onChange={(e) => setSearchQuery(e.target.value)}
                 />
-                <button onClick={() => navigate('/listings')}>
-                  Continue Shopping
-                </button>
+                <button onClick={() => navigate('/listings')}>Continue Shopping</button>
               </div>
 
               <table>
@@ -74,7 +88,8 @@ function Cart() {
                   <tr>
                     <th className="col-2"></th>
                     <th className="col-3"></th>
-                    <th className="col-5">SELECTED ITEMS</th>
+                    <th className="col-3">SELECTED ITEMS</th>
+                    <th className="col-2">QUANTITY</th>
                     <th className="col-2">PRICE</th>
                   </tr>
                 </thead>
@@ -99,8 +114,25 @@ function Cart() {
                           alt="shop-img"
                         />
                       </td>
-                      <td className="col-5">
+                      <td className="col-3">
                         <span>{item?.title}</span>
+                      </td>
+                      <td className="col-2 counter-td">
+                        <div className="count">
+                          <button
+                            className="counter-btn"
+                            onClick={() => handleDecrement(item)}
+                          >
+                            <FaMinus />
+                          </button>
+                          <span className="counter">{item.count}</span>
+                          <button
+                            className="counter-btn"
+                            onClick={() => handleIncrement(item)}
+                          >
+                            <FaPlus />
+                          </button>
+                        </div>
                       </td>
                       <td className="col-2">
                         <span>${item?.price}</span>
@@ -137,3 +169,159 @@ function Cart() {
 }
 
 export default Cart;
+
+
+// import React, { useEffect, useState } from 'react';
+// import { Helmet } from 'react-helmet';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { removeFromCart } from '../../../redux/ShoppingSlice/ShoppingSlice';
+// import Loading from '../../../components/Site/Loading/Loading';
+// import './Cart.scss';
+// import { Link, useNavigate } from 'react-router-dom';
+// import { RiDeleteBin6Line } from 'react-icons/ri';
+// import { toast, ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import { FaPlus , FaMinus } from 'react-icons/fa'
+
+// function Cart() {
+//   const items = useSelector((state) => state.shoppingcart.items);
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+
+//   const [loading, setLoading] = useState(false);
+//   const [searchQuery, setSearchQuery] = useState('');
+
+//   useEffect(() => {
+//     setLoading(true);
+//     window.scrollTo({ top: 0 });
+//     setTimeout(() => {
+//       setLoading(false);
+//     }, 1500);
+//   }, [items]);
+
+//   const filteredItems = items.filter((item) =>
+//     item.title.toLowerCase().includes(searchQuery.toLowerCase())
+//   );
+
+//   let totalPrice = 0;
+//   filteredItems.forEach((item) => {
+//     totalPrice += item.price;
+//   });
+
+//   return (
+//     <>
+//       {loading ? (
+//         <Loading />
+//       ) : (
+//         <div>
+//           <Helmet>
+//             <title>Cart</title>
+//           </Helmet>
+//           <div className="cart">
+//             <div className="cart__top">
+//               <div className="cart__top__wrapper">
+//                 <div className="cart__top__wrapper__text">
+//                   <h2>CART</h2>
+//                   <p>
+//                     <Link to="/">Home</Link>/Cart
+//                   </p>
+//                 </div>
+//               </div>
+//             </div>
+
+//             <div className="cart__products">
+//               <div className="cart__products__top">
+//                 <input
+//                   type="text"
+//                   placeholder="Search..."
+//                   value={searchQuery}
+//                   onChange={(e) => setSearchQuery(e.target.value)}
+//                 />
+//                 <button onClick={() => navigate('/listings')}>
+//                   Continue Shopping
+//                 </button>
+//               </div>
+
+//               <table>
+//                 {/* header */}
+//                 <thead>
+//                   <tr>
+//                     <th className="col-2"></th>
+//                     <th className="col-3"></th>
+//                     <th className="col-3">SELECTED ITEMS</th>
+//                     <th className="col-2">QUANTITY</th>
+//                     <th className="col-2">PRICE</th>
+//                   </tr>
+//                 </thead>
+
+//                 {/* body */}
+//                 <tbody>
+//                   {filteredItems.map((item, index) => (
+//                     <tr key={index}>
+//                       <td className="col-2">
+//                         <button
+//                           className="table-delete"
+//                           onClick={() => {
+//                             dispatch(removeFromCart(item.id));
+//                           }}
+//                         >
+//                           <RiDeleteBin6Line />
+//                         </button>
+//                       </td>
+//                       <td className="col-3">
+//                         <img
+//                           src={`http://localhost:8080/public/${item?.image}`}
+//                           alt="shop-img"
+//                         />
+//                       </td>
+//                       <td className="col-3">
+//                         <span>{item?.title}</span>
+//                       </td>
+//                       <td className="col-2 counter-td">
+//                         <div className="count">
+//                           <span>
+//                             <FaMinus />
+//                           </span>
+//                           <span className='counter'>
+//                             0
+//                           </span>
+//                           <span>
+//                             <FaPlus />
+//                           </span>
+//                         </div>
+//                       </td>
+//                       <td className="col-2">
+//                         <span>${item?.price}</span>
+//                       </td>
+//                     </tr>
+//                   ))}
+//                 </tbody>
+//               </table>
+
+//               <div className="cart__total">
+//                 <h2>Cart Totals</h2>
+//                 <table>
+//                   <thead>
+//                     <tr>
+//                       <th>Subtotal</th>
+//                       <td>${totalPrice}</td>
+//                     </tr>
+//                     <tr>
+//                       <th>Total</th>
+//                       <td>${totalPrice}</td>
+//                     </tr>
+//                   </thead>
+//                 </table>
+//                 <div className="total-btn">
+//                   <button>Proceed to Checkout</button>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       )}
+//     </>
+//   );
+// }
+
+// export default Cart;
--- frontend/src/redux/ShoppingSlice/ShoppingSlice.js
+++ frontend/src/redux/ShoppingSlice/ShoppingSlice.js
@@ -1,22 +1,61 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const ShoppingSlice = createSlice({
+const shoppingSlice = createSlice({
     name: "shoppingcart",
     initialState: {
-        items: []
+        items: [],
     },
     reducers: {
         addToCart: (state, action) => {
-            // id == payload
-            state.items.push(action.payload)
-            console.log(state.items)
+            const newItem = action.payload;
+            const existingItem = state.items.find((item) => item.id === newItem.id);
+            if (existingItem) {
+                existingItem.count += 1;
+            } else {
+                state.items.push({ ...newItem, count: 1 });
+            }
         },
         removeFromCart: (state, action) => {
-            let target = state.items.find(item => item.id == action.payload)
-            let indexOftarget = state.items.indexOf(target)
-            state.items.splice(indexOftarget, 1)
-        }
-    }
-})
-export const { addToCart, removeFromCart } = ShoppingSlice.actions
-export default ShoppingSlice.reducer
+            const itemId = action.payload;
+            const index = state.items.findIndex((item) => item.id === itemId);
+            if (index !== -1) {
+                const item = state.items[index];
+                if (item.count === 1) {
+                    state.items.splice(index, 1);
+                } else {
+                    item.count -= 1;
+                }
+            }
+        },
+    },
+});
+
+export const { addToCart, removeFromCart } = shoppingSlice.actions;
+export default shoppingSlice.reducer;
+
+
+
+
+
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const ShoppingSlice = createSlice({
+//     name: "shoppingcart",
+//     initialState: {
+//         items: []
+//     },
+//     reducers: {
+//         addToCart: (state, action) => {
+//             // id == payload
+//             state.items.push(action.payload)
+//             console.log(state.items)
+//         },
+//         removeFromCart: (state, action) => {
+//             let target = state.items.find(item => item.id == action.payload)
+//             let indexOftarget = state.items.indexOf(target)
+//             state.items.splice(indexOftarget, 1)
+//         }
+//     }
+// })
+// export const { addToCart, removeFromCart } = ShoppingSlice.actions
+// export default ShoppingSlice.reducer
